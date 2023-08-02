ea2ab21cac484e9e6243d8b843c61e5c2b5656e2
Aliphonza Nderitu <aliphonzanderitu@gmail.com>
2023-08-02 13:18:14 +0300

    v

--- db/db.go
+++ db/db.go
@@ -31,7 +31,7 @@ func MoReport() {
 	orgID := 209
 	src := os.Getenv("SHORTCODE")
 	// SQL query to retrieve data created today
-	query := fmt.Sprintf("SELECT network, mcc, mnc, cc, msisdn, flow, src_address, created_on FROM tbl_campaign_messages WHERE flow='%s' AND org_id='%d' AND DATE(created_on)='%s' AND src_address = '%v';", FLOW, orgID, yesterdayStr, src)
+	query := fmt.Sprintf("SELECT network, mcc, mnc, cc, msisdn, flow, src_address, created_on FROM tbl_campaign_messages WHERE flow='%s' AND org_id='%d' AND DATE(created_on)='%s' AND src_address = '%s';", FLOW, orgID, yesterdayStr, src)
 
 	// Connect to the database
 	db, err := sql.Open("mysql", fmt.Sprintf("%s:%s@tcp(%s:%s)/%s", username, password, host, port, dbName))
